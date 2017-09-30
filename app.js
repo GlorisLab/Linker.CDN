1f1a5a52bcbee79f51744e16d8070cd8ac681c98
alexander <st235@yandex.ru>
2017-09-30 14:25:05 +0300

    Implemented cdn

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var path = require('path')
+var express = require('express')
+var serveStatic = require('serve-static')
+
+var app = express()
+
+app.use(serveStatic(path.join(__dirname, 'public')))
+
+app.listen(3000, () => console.info('App listen on 3000'))
