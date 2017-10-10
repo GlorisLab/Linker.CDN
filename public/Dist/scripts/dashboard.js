df00eecf21f762a21f8b7b1757c5b6cf45d674aa
Denis Korokhodkin <korokhodkindenis@gmail.com>
2017-10-10 12:15:21 +0300

    builds

---
+++ public/Dist/scripts/dashboard.js
@@ -0,0 +1,51 @@
+webpackJsonp([1],{
+
+/***/ 350:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _AppService = __webpack_require__(203);
+
+var _AppService2 = _interopRequireDefault(_AppService);
+
+var _Reducers = __webpack_require__(509);
+
+var _Reducers2 = _interopRequireDefault(_Reducers);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+new _AppService2.default({
+  container: React.createElement(
+    'div',
+    null,
+    'asf'
+  ),
+  reducers: _Reducers2.default,
+  DOMNode: document.getElementById('root')
+}).init();
+
+/***/ }),
+
+/***/ 509:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _redux = __webpack_require__(48);
+
+var _reduxForm = __webpack_require__(28);
+
+exports.default = (0, _redux.combineReducers)({
+  form: _reduxForm.reducer
+});
+
+/***/ })
+
+},[350]);
